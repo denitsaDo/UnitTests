18e4fe013f3dd6b00ae6f1eacdf4201c4a41054f
Denitsa Dochkova <Denitsa.Dochkova@gmail.com>
2022-06-14 11:31:09 +0300

    Test with assertThrows

---
+++ test/patientintake/DateTimeConverterShould.java
@@ -0,0 +1,33 @@
+package patientintake;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTimeConverterShould {
+    @Test
+    void convertTodayStringCorrectrly() {
+        LocalDateTime result = DateTimeConverter.convertStringToDateTime("today 1:00 pm", LocalDate.of(2018, 9 , 1));
+        assertEquals(result, LocalDateTime.of(2018,9,1,13,0));
+    }
+
+    @Test
+    void convertCorrectPatternToDateTime() {
+        LocalDateTime result = DateTimeConverter.convertStringToDateTime("09/02/2018 1:00 pm", LocalDate.of(2018, 9 , 1));
+        assertEquals(result, LocalDateTime.of(2018,9,2,13,0));
+    }
+
+    @Test
+    void throwExceptionIfIncorrectPatternProvided() {
+        Throwable error = assertThrows(RuntimeException.class, () ->
+                DateTimeConverter.convertStringToDateTime("09/02/2018 100 pm",
+                        LocalDate.of(2018, 9 , 1)));
+        assertEquals("Unable to create date time from: [09/02/2018 100 PM], " +
+                "please enter with format [M/d/yyyy h:mm a]Text '09/02/2018 100 PM' " +
+                "could not be parsed at index 14", error.getMessage());
+    }
+
+}
